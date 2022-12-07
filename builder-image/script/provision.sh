69a44fff5913184807e20f24446e5ed3ea92ded8
Jon Seager <jon@sgrs.uk>
2022-12-07 13:26:32 +0000

    feat: enable using dhcp with firecracker vms (#1)

--- builder-image/script/provision.sh
+++ builder-image/script/provision.sh
@@ -6,42 +6,6 @@ fi
 
 dpkg -i /mnt/root/linux*.deb
 
-# TODO: remove this when cloud-init is working
-echo "ubuntu" > /etc/hostname
-
-# This essentially makes root passwordless - seems like a bad idea ;-)
-# TODO: Change this once cloud-init provisioning works
-passwd -d root
-
-# TODO: Remove the autologin once cloud-init provisioning is working
-mkdir /etc/systemd/system/serial-getty@ttyS0.service.d/
-cat <<EOF > /etc/systemd/system/serial-getty@ttyS0.service.d/autologin.conf
-[Service]
-ExecStart=
-ExecStart=-/sbin/agetty --autologin root -o '-p -- \\u' --keep-baud 115200,38400,9600 %I xterm-256color
-EOF
-
-# Configure networking using netplan.
-# TODO: Work out how to setup dhcp on the tap interface and simplify this
-cat <<EOF > /etc/netplan/99_config.yaml
-network:
-  version: 2
-  renderer: networkd
-  ethernets:
-    eth0:
-      dhcp4: false
-      addresses:
-        - 172.20.0.2/24
-      routes:
-        - to: default
-          via: 172.20.0.1
-      nameservers:
-        addresses:
-          - 1.1.1.1
-EOF
-
-netplan generate
-
 # Configure subgid/subuid for LXD
 cat <<EOF > /etc/subuid
 lxd:1000000:65536
@@ -49,16 +13,30 @@ root:1000000:65536
 EOF
 cp /etc/subuid /etc/subgid
 
+# TODO: Remove below this line when cloud-init provisioning is working
+
+# TODO: remove this when cloud-init is working
+# echo "ubuntu" > /etc/hostname
+
+# This essentially makes root passwordless - seems like a bad idea ;-)
+# TODO: Change this once cloud-init provisioning works
+passwd -d root
+
+mkdir /etc/systemd/system/serial-getty@ttyS0.service.d/
+cat <<EOF > /etc/systemd/system/serial-getty@ttyS0.service.d/autologin.conf
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin root -o '-p -- \\u' --keep-baud 115200,38400,9600 %I xterm-256color
+EOF
+
 # Add the alacritty terminfo profile
-# TODO: Remove this when cloud-init provisioning is working
 wget -q https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.info
 tic -xe alacritty,alacritty-direct ./alacritty.info
 rm alacritty.info
 
-# Create the ubuntu user
+# Add the ubuntu user and an ssh key
 useradd -m -s /bin/bash ubuntu
 mkdir -p /home/ubuntu/.ssh
-# TODO: Remove this when cloud-init provisioning is working
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMnd4bqCUEzrVkQBTVbQOKVBozJ2ZNJUFvWJFhLc7cST jnsgruk" >> /home/ubuntu/.ssh/authorized_keys
 
 # Setup NOPASSWD sudo access for the ubuntu user
--- demo.sh
+++ demo.sh
@@ -4,10 +4,15 @@ if [[ -n "${DEBUG:-}" ]]; then
     set -x
 fi
 
-# Specs for the VM
-CPUS="8"
-MEMORY="16386" # 16GB
-DISK="20G"
+# Grab the machine config from a file
+source "${FC_CONFIG:-default.conf}"
+
+# Check for some dependencies
+command -v firecracker >/dev/null || (echo "could not find firecracker in PATH"; exit 1)
+command -v firectl >/dev/null || (echo "could not find firectl in PATH"; exit 1)
+if [[ -z "${FC_DHCP:-}" ]]; then
+  command -v dnsmasq >/dev/null || (echo "could not find dnsmasq in PATH"; exit 1)
+fi
 
 # Check if the linux kernel and disk images are built, and build them if not
 if [[ ! -f "./build/dist/vmlinux" ]] || [[ ! -f "./build/dist/image.ext4" ]]; then
@@ -21,29 +26,88 @@ if [[ ! -d "./vm" ]]; then
   # Copy image and kernel
   cp build/dist/vmlinux build/dist/image.ext4 vm/
   # Resize image to 20G
-  truncate -s "$DISK" vm/image.ext4
+  truncate -s "$FC_DISK" vm/image.ext4
   resize2fs vm/image.ext4
 fi
 
-TAP_IFACE="fc-tap0"
-DEVICE_NAME="enp39s0"
+# Kernel command line for the VM
+KERNEL_OPTS="init=/bin/systemd noapic reboot=k panic=1 pci=off console=ttyS0 systemd.hostname=${FC_HOSTNAME}"
 
-# Do some network interface setup if not already complete
-if ! ip l | grep -q "$TAP_IFACE"; then
-  sudo ip tuntap add dev "$TAP_IFACE" mode tap user "$(whoami)"
-  sudo ip addr add 172.20.0.1/24 dev "$TAP_IFACE"
-  sudo ip link set "$TAP_IFACE" up
-  sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
-  sudo iptables -t nat -A POSTROUTING -o "$DEVICE_NAME" -j MASQUERADE
-  sudo iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-  sudo iptables -A FORWARD -i "$TAP_IFACE" -o "$DEVICE_NAME" -j ACCEPT
+# Get the device name of the default gateway on the host
+DEVICE_NAME="$(\ip route get 8.8.8.8 |grep uid |sed "s/.* dev \([^ ]*\) .*/\1/")"
+# Generate a name for the tap interface
+TAP_IFACE="fctap0$(openssl rand -hex 2)"
+
+if [[ -z "${FC_DHCP:-}" ]]; then
+  # Do some network interface setup if not already complete
+  if ! ip l | grep -q "$TAP_IFACE"; then
+    sudo ip tuntap add dev "$TAP_IFACE" mode tap user "$(whoami)"
+    sudo ip addr add 172.20.0.1/24 dev "$TAP_IFACE"
+    sudo ip link set "$TAP_IFACE" up
+    sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+    sudo iptables -t nat -A POSTROUTING -o "$DEVICE_NAME" -j MASQUERADE
+    sudo iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+    sudo iptables -A FORWARD -i "$TAP_IFACE" -o "$DEVICE_NAME" -j ACCEPT
+  fi
+
+  KERNEL_OPTS="$KERNEL_OPTS ip=172.20.0.2::172.20.0.1:255.255.255.0::eth0:off"
+else
+  BR_IFACE="fcbr0"
+  
+  # If there is no bridge interface, create one
+  if ! ip l | grep -q "$BR_IFACE"; then
+    sudo ip link add name "$BR_IFACE" type bridge
+    sudo ip addr add 172.20.0.1/24 dev "$BR_IFACE"
+    sudo ip link set "$BR_IFACE" up
+
+    sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+    sudo iptables -t nat -A POSTROUTING -o "$DEVICE_NAME" -j MASQUERADE
+    sudo iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+    sudo iptables -A FORWARD -i "$BR_IFACE" -o "$DEVICE_NAME" -j ACCEPT
+  fi
+
+  # Create a tap interface for the specific VM, add it to the bridge
+  if ! ip l | grep -q "$TAP_IFACE"; then
+    sudo ip tuntap add dev "$TAP_IFACE" mode tap # user "$(whoami)"
+    sudo ip link set "$TAP_IFACE" master "$BR_IFACE"
+    sudo ip link set "$TAP_IFACE" up
+  fi
+
+  # Start dnsmasq for DHCP/DNS on the bridge
+  sudo dnsmasq \
+    --strict-order \
+    --bind-interfaces \
+    --log-facility="$(pwd)/dnsmasq.log" \
+    --pid-file="$(pwd)/dnsmasq.pid" \
+    --dhcp-leasefile="$(pwd)/dnsmasq.leases" \
+    --dhcp-hostsfile="$(pwd)/dnsmasq.hosts" \
+    --domain=firecracker \
+    --local=/firecracker/ \
+    --except-interface=lo \
+    --interface="$BR_IFACE" \
+    --listen-address=172.20.0.1 \
+    --dhcp-no-override \
+    --dhcp-authoritative \
+    --dhcp-range 172.20.0.2,172.20.0.100,infinite
+
+  KERNEL_OPTS="$KERNEL_OPTS ip=dhcp"
 fi
 
 # Start the VM
 firectl \
-    --ncpus "$CPUS" \
-    --memory "$MEMORY" \
+    --ncpus "$FC_CPUS" \
+    --memory "$FC_MEMORY" \
     --kernel="vm/vmlinux" \
     --root-drive="vm/image.ext4" \
-    --tap-device="$TAP_IFACE/$(cat /sys/class/net/$TAP_IFACE/address)" \
-    --kernel-opts="init=/bin/systemd noapic reboot=k panic=1 pci=off console=ttyS0"
+    --tap-device="$TAP_IFACE/$(cat "/sys/class/net/$TAP_IFACE/address")" \
+    --kernel-opts="$KERNEL_OPTS"
+
+
+# Cleanup and remove interfaces once the machine has gone down
+sudo ip link del "$TAP_IFACE"
+
+if [[ -n "${FC_DHCP:-}" ]]; then
+  sudo kill -9 "$(cat dnsmasq.pid)"
+  sudo ip link del "$BR_IFACE"
+  sudo rm "$(pwd)/dnsmasq.pid"
+fi
