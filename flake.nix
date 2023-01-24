4c8d244b9c44b48667e14f5f1ca08cb3d4760af4
Jon Seager <jon@sgrs.uk>
2023-01-24 19:38:01 +0000

    build: add flake for nix package manager (#3)
    * build: add basic flake.nix
    
    * fix: default value for XDG_RUNTIME_DIR
    
    * feat: don't use script's directory for storing files
    
    Previously, embr used the base directory where embr was
    located for storing downloaded images, vms, config, etc.
    
    This was fine in early development, but for installs where
    embr might be in /usr/bin or the nix store, this is not a
    great plan.
    
    This change introduces $HOME/.config/embr as the default root
    directory for storing files and config.
    
    * build: add dependencies to flake; fix yq usage
    
    * feat: include the vmlinux script in the nix flake

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "embr - simple automation for booting Ubuntu microVMs with firecracker";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+
+        deps = with pkgs; [
+          bash
+          curl
+          dnsmasq
+          e2fsprogs
+          firecracker
+          iptables
+          jq
+          killall
+          openssh
+          openssl
+          yq
+          # The following are dependencies of the extract-vmlinuz script
+          binutils
+          bzip2
+          gzip
+          lzip
+          lzop
+          lz4
+          zstd
+          unzip
+          xz
+        ];
+
+        embr = (pkgs.writeScriptBin "embr" (builtins.readFile ./embr)).overrideAttrs (old: {
+          buildCommand = "${old.buildCommand}\n patchShebangs $out";
+        });
+
+        extract-vmlinux =
+          (pkgs.writeScriptBin "extract-vmlinux"
+            (builtins.readFile ./util/extract-vmlinux))
+          .overrideAttrs (
+            old: {
+              buildCommand = "${old.buildCommand}\n patchShebangs $out";
+            }
+          );
+      in rec {
+        defaultPackage = packages.embr;
+
+        packages.embr = pkgs.symlinkJoin {
+          name = "embr";
+          paths = [embr extract-vmlinux] ++ deps;
+          buildInputs = [pkgs.makeWrapper];
+          postBuild = "wrapProgram $out/bin/embr --prefix PATH : $out/bin";
+        };
+      }
+    );
+}
