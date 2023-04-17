5f3a5b16a87ddf93149dcbdaa8b697ffbc34a66e
Jon Seager <jon@sgrs.uk>
2023-04-17 18:10:26 +0100

    refactor: change structure of flake.nix to include overlay

--- flake.nix
+++ flake.nix
@@ -1,61 +1,73 @@
 {
   description = "embr - simple automation for booting Ubuntu microVMs with firecracker";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs =
+    { self
+    , nixpkgs
+    , ...
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        # "aarch64-linux"
+        # "x86_64-darwin"
+        # "aarch64-darwin"
+      ];
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {inherit system;};
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
 
-        deps = with pkgs; [
-          curl
-          dnsmasq
-          e2fsprogs
-          firecracker
-          iptables
-          jq
-          killall
-          openssh
-          openssl
-          yq
-          # The following are dependencies of the extract-vmlinuz script
-          binutils
-          bzip2
-          gzip
-          lzip
-          lzop
-          lz4
-          zstd
-          unzip
-          xz
-        ];
-
-        embr = (pkgs.writeScriptBin "embr" (builtins.readFile ./embr)).overrideAttrs (old: {
+      pkgsForSystem = system: (import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      });
+    in
+    rec {
+      overlay = final: prev: {
+        embr-unwrapped = (final.writeScriptBin "embr" (builtins.readFile ./embr)).overrideAttrs (old: {
           buildCommand = "${old.buildCommand}\n patchShebangs $out";
         });
 
-        extract-vmlinux =
-          (pkgs.writeScriptBin "extract-vmlinux"
-            (builtins.readFile ./util/extract-vmlinux))
-          .overrideAttrs (
-            old: {
-              buildCommand = "${old.buildCommand}\n patchShebangs $out";
-            }
-          );
-      in rec {
-        defaultPackage = packages.embr;
+        extract-vmlinux-unwrapped = (final.writeScriptBin "extract-vmlinux" (builtins.readFile ./util/extract-vmlinux)).overrideAttrs (old: {
+          buildCommand = "${old.buildCommand}\n patchShebangs $out";
+        });
 
-        packages.embr = pkgs.symlinkJoin {
+        embr = final.symlinkJoin {
           name = "embr";
-          paths = [embr extract-vmlinux] ++ deps;
-          buildInputs = [pkgs.makeWrapper];
+          paths = (with final; [
+            embr-unwrapped
+            extract-vmlinux-unwrapped
+          ])
+          ++ (with final; [
+            curl
+            dnsmasq
+            e2fsprogs
+            firecracker
+            iptables
+            jq
+            killall
+            openssh
+            openssl
+            yq
+            # The following are dependencies of the extract-vmlinuz script
+            binutils
+            bzip2
+            gzip
+            lzip
+            lzop
+            lz4
+            zstd
+            unzip
+            xz
+          ]);
+          buildInputs = [ final.makeWrapper ];
           postBuild = "wrapProgram $out/bin/embr --prefix PATH : $out/bin";
         };
-      }
-    );
+      };
+
+      packages = forAllSystems (system: {
+        inherit (pkgsForSystem system) embr;
+      });
+
+      defaultPackage = forAllSystems (system: (pkgsForSystem system).embr);
+    };
 }
